772a2e965a12a85272ff5660f7e9f2840a182b1f
jbeatty88 <joshuabeatty88@gmail.com>
2018-11-01 23:22:17 +0000

    snow station map app

--- Section2/NodeCreative/app/routes/index.js
+++ Section2/NodeCreative/app/routes/index.js
@@ -1,12 +1,28 @@
 var express = require('express');
 var router = express.Router();
-var fs = require("fs");
-var request = require("request");
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.sendFile('home.html', {root: 'public' });
-//   res.render('index', { title: 'Express' });
+    res.sendFile('index.html', { root: 'public' });
 });
+
+router.get('/map', function(req, res, next) {
+    res.sendFile('map.html', { root: 'public' });
+})
+
+router.get('/powderline', function(req, res, next)
+{
+    var url = "http://api.powderlin.es/closest_stations?"
+    url += "lat=" + req.query.lat;
+    url += "&lng=" + req.query.lng;
+    url += "&data=true&days=3&count=1";
+    console.log(url);
+
+    request(url).pipe(res);
+    console.log("piped");
+});
+
+
 
 module.exports = router;
